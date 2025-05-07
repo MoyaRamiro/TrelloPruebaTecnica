a05a5aa5e0059b50241c2d222e1461a54970c6c9
MoyaRamiro <moya04.ramiro@gmail.com>
2025-05-07 04:01:39 -0300

    Agregar componente Board para gestión de tarjetas con funcionalidad de arrastrar y soltar, y opción para añadir y eliminar tarjetas.

---
+++ src/app/board.tsx
@@ -0,0 +1,129 @@
+import { BoardItem } from "@/types/boardItem";
+import { useDragAndDrop } from "@formkit/drag-and-drop/react";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+interface BoardListProps {
+  title: string;
+  elements: BoardItem[];
+}
+
+const Board = ({ title, elements }: BoardListProps) => {
+  const [cardList, cards, setCards] = useDragAndDrop<
+    HTMLUListElement,
+    BoardItem
+  >(elements, {
+    group: "cardList",
+    multiDrag: true,
+    dragHandle: ".kanban-handle",
+    selectedClass: "rounded-lg p-0.5 rotate-1",
+    draggable: (el) => {
+      return el.id !== "no-drag";
+    },
+  });
+
+  const [newName, setNewName] = useState("");
+
+  const removeCard = (id: string, list: BoardItem[]) => {
+    const newList = list.filter((item) => item.id !== id);
+    setCards(newList);
+  };
+
+  const addCard = (name: string) => {
+    const newCard = { id: uuidv4(), name };
+    setCards([...cards, newCard]);
+  };
+
+  return (
+    <div className="my-5 mx-1 p-5 w-96">
+      <div className=" bg-gray-800 rounded-xl p-5 ">
+        <h2 className="break-all opacity-90 text-2xl font-bold pb-3">
+          {title}
+        </h2>
+        <ul ref={cardList}>
+          {cards.map((card) => (
+            <li className="cassette" data-label={card} key={card.id}>
+              <div className="kanban-handle flex items-center w-full max-w-xs p-3.5 text-gray-300 bg-gray-700 my-2 rounded-xl hover:border-2 hover:border-gray-500">
+                <div className="flex">
+                  <input
+                    type="checkbox"
+                    value=""
+                    className="w-4 h-4 rounded-full bg-gray-700 border-gray-600 text-blue-600 focus:ring-blue-500 focus:ring-2"
+                  />
+                  <label htmlFor="default-checkbox" className=""></label>
+                </div>
+
+                <span className="ms-3 text-sm font-normal break-all">
+                  {card.name}
+                </span>
+                <button
+                  type="button"
+                  className="cursor-pointer  ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-700 dark:hover:bg-gray-700"
+                  onClick={() => removeCard(card.id, cards)}
+                >
+                  <svg className="w-3 h-3">
+                    <use href="icons.svg#cross" />
+                  </svg>
+                </button>
+              </div>
+            </li>
+          ))}
+
+          <div id="no-drag" className="AddCard">
+            <label
+              htmlFor={title}
+              className="text-gray-400 hover:text-gray-900 rounded-lg w-fit dark:text-gray-500 focus:text-black cursor-pointer"
+            >
+              <div className="flex items-center w-full p-2 max-w-xs text-gray-300 bg-gray-800 rounded-xl hover:bg-gray-700">
+                <input type="checkbox" id={title} className="hidden peer" />
+
+                <svg className="w-6 h-6 mr-1.5 items-center peer-checked:hidden hover:text-gray-900">
+                  <use href="icons.svg#add" />
+                </svg>
+
+                <label
+                  htmlFor={title}
+                  className="mr-1.5 text-gray-400 hover:text-gray-900 rounded-lg p-1.5 w-fit h-8 dark:text-gray-500 focus:text-black cursor-pointer hidden peer-checked:block"
+                >
+                  <svg className="w-4 h-4 items-center">
+                    <use href="icons.svg#cross" />
+                  </svg>
+                </label>
+
+                <span className="w-fit mt-2 peer-checked:hidden text-gray-400 cursor-pointer">
+                  Añadir tarjeta...
+                </span>
+
+                <div className="flex-col w-full hidden peer-checked:flex">
+                  <form>
+                    <textarea
+                      className="border-gray-700 border-2 w-full bg-transparent text-gray-300 focus:outline-none resize-y overflow-hidden px-5 pt-1 rounded-lg"
+                      placeholder="Escribe el título de la tarjeta..."
+                      rows={2}
+                      value={newName}
+                      onChange={(e) => setNewName(e.target.value)}
+                    />
+                    <div className="flex justify-end w-full">
+                      <label
+                        className="mr-5 rounded-lg bg-transparent text-gray-300 cursor-pointer hover:bg-gray-900 w-fit p-2"
+                        htmlFor={title}
+                        onClick={() => {
+                          addCard(newName);
+                          setNewName("");
+                        }}
+                      >
+                        Añadir
+                      </label>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </label>
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Board;
